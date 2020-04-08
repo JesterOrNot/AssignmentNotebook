02daeaa920a6f3299674a9b362d92be150a07084
Sean Hellum <seanhellum45@gmail.com>
2020-04-08 01:12:58 +0000

    Update UI

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello World',
+      debugShowCheckedModeBanner: false,
+      title: 'Assignment Notebook',
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Hello World"),
-        ),
+            title: Text("Assignment Notebook"),
+            shape: new CircleBorder(side: BorderSide(width: 59))),
         body: Center(
-          child: Text('Hello World From Flutter'),
+          child: Text(
+            'Hello World From Flutter',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
         ),
       ),
     );
